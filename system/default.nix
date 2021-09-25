a0f5011798d7e05968a19f0f167daf82ac049b40
poultryghast <murp8752@gmail.com>
2021-09-25 18:39:37 -0400

    updated discord version

--- system/default.nix
+++ system/default.nix
@@ -61,7 +61,14 @@
       haskellPackages.xmobar rofi
       spotify playerctl vlc
       firefox
-      discord slack
+      (pkgs.discord.overrideAttrs (old: rec {
+        version = "0.0.16";
+	src = fetchurl {
+	  url = "https://dl.discordapp.net/apps/linux/${version}/discord-${version}.tar.gz";
+	  sha256 = "1s9qym58cjm8m8kg3zywvwai2i3adiq6sdayygk2zv72ry74ldai";
+	};
+      }))
+      slack
       alacritty
       git neovim nodejs
       minecraft multimc
